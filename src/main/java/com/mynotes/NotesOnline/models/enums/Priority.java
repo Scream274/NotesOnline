006694d7a0b0f70693c49a3b1510f0723ef75de6
Anatolii <omelchenko20@gmail.com>
2024-02-17 22:37:14 +0200

    feat: styled html-pages

--- src/main/java/com/mynotes/NotesOnline/models/enums/Priority.java
+++ src/main/java/com/mynotes/NotesOnline/models/enums/Priority.java
@@ -1,5 +1,23 @@
 package com.mynotes.NotesOnline.models.enums;
 
 public enum Priority {
-    LOW, MEDIUM, HIGH
+    LOW("green", "#cfffcf"),
+    MEDIUM("goldenrod", "#ffffd4"),
+    HIGH("red", "#fff3f3");
+
+    private final String colorCode;
+    private final String backColorCode;
+
+    Priority(String colorCode, String backColorCode) {
+        this.colorCode = colorCode;
+        this.backColorCode = backColorCode;
+    }
+
+    public String getColorCode() {
+        return colorCode;
+    }
+
+    public String getBackColorCode() {
+        return backColorCode;
+    }
 }
