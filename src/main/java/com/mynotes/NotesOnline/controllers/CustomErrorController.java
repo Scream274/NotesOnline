489adc721d2b515a9d3ce4e596ca5ad810b40176
Anatolii <omelchenko20@gmail.com>
2024-02-21 13:25:42 +0200

    feat: added CustomErrorController for 404, 403 and 500 errors.

---
+++ src/main/java/com/mynotes/NotesOnline/controllers/CustomErrorController.java
@@ -0,0 +1,30 @@
+package com.mynotes.NotesOnline.controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        var status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "404_page";
+            } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "access_denied_page";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "500_page";
+            }
+        }
+        return "404_page";
+    }
+}
